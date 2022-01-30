0db8e8a0ecfbbf4dcaa0ea9e173ef3b54d789a2a
Sam <sam.rae@simpplr.com>
2022-01-30 21:21:08 +0000

    add react router

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,22 +1,63 @@
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 const RemoteApp = React.lazy(() => import("app2/App"));
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/about" element={<About />} />
+
+          <Route path="/users" element={<Users />} />
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+function Home() {
+  return (
     <div>
-      <div style={{
-        margin:"10px",
-        padding:"10px",
-        textAlign:"center",
-        backgroundColor:"greenyellow"
-      }}>
+      <div
+        style={{
+          margin: "10px",
+          padding: "10px",
+          textAlign: "center",
+          backgroundColor: "greenyellow",
+        }}
+      >
         <h1>App1</h1>
       </div>
       <Suspense fallback={"loading..."}>
-        <RemoteApp/>
+        <RemoteApp />
       </Suspense>
-    </div>)
+    </div>
+  );
 }
 
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 
 export default App;
